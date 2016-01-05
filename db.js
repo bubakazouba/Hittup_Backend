a2620cf75668c78acdf9888efda59a7c210020eb
fantaandcrackers <arthur.shir@gmail.com>
2016-01-04 20:20:33 -0800

    Readded mongodb connection

--- db.js
+++ db.js
@@ -1,3 +1,8 @@
+// Connect Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://Hittup:katyCherry1738@ds043981.mongolab.com:43981/hittup'); // connect to our database
+
+
 // Connect MongoClient
 var mongoClient = require('mongodb').MongoClient;
 mongoClient.connect("mongodb://Hittup:katyCherry1738@ds043981.mongolab.com:43981/hittup", function(err, db) {
